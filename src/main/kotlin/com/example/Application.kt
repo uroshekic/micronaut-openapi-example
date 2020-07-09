648e6d8002d27f30b8441ceb387fe4cb58687de7
Uroš Hekić <uros.hekic@marand.si>
2020-07-09 18:32:00 +0200

    Add and configure Micronaut OpenAPI

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,7 +1,20 @@
 package com.example
 
-import io.micronaut.runtime.Micronaut.build
+import io.micronaut.runtime.Micronaut.*
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 
+@OpenAPIDefinition(
+		info = Info(
+				title = "Hello World",
+				version = "1.0",
+				description = "My API",
+				license = License(name = "Apache 2.0", url = "https://foo.bar"),
+				contact = Contact(url = "https://company.com", name = "Support", email = "support@company.com")
+		)
+)
 object ApplicationKt {
 
 	@JvmStatic
